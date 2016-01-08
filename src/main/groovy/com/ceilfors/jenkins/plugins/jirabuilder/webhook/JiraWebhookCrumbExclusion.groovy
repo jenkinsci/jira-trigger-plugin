69320696841b7c8a6622b8dcbe6b36844105c201
ceilfors <wisen@ceilfors.com>
2016-01-08 16:37:51 +0000

    Handle pathInfo with or without trailing slash instead of logging them.

--- src/main/groovy/com/ceilfors/jenkins/plugins/jirabuilder/webhook/JiraWebhookCrumbExclusion.groovy
+++ src/main/groovy/com/ceilfors/jenkins/plugins/jirabuilder/webhook/JiraWebhookCrumbExclusion.groovy
@@ -1,4 +1,5 @@
 package com.ceilfors.jenkins.plugins.jirabuilder.webhook
+
 import hudson.Extension
 import hudson.security.csrf.CrumbExclusion
 
@@ -6,30 +7,23 @@ import javax.servlet.FilterChain
 import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-import java.util.logging.Logger
 /**
  * @author ceilfors
  */
 @Extension
 class JiraWebhookCrumbExclusion extends CrumbExclusion {
 
-    private static final Logger LOGGER = Logger.getLogger(JiraWebhookCrumbExclusion.name)
-
     @Override
     boolean process(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         String pathInfo = request.getPathInfo()
-        if (pathInfo != null && pathInfo.equals(getExclusionPath())) {
+        if (pathInfo != null && (pathInfo.equals(exclusionPath) || pathInfo.equals(exclusionPath + "/"))) {
             chain.doFilter(request, response)
             return true
-        } else {
-            if (pathInfo.contains(JiraWebhook.URLNAME)) {
-                LOGGER.finest("Ignoring pathInfo $pathInfo even when it contains $JiraWebhook.URLNAME")
-            }
-            return false
         }
+        return false
     }
 
     String getExclusionPath() {
-        return "/" + JiraWebhook.URLNAME + "/"
+        return "/" + JiraWebhook.URLNAME
     }
 }
