4300ee10f73c88c4d4d5a40220137916ac715c93
Wisen Tanasa <wisen@ceilfors.com>
2016-10-25 07:05:44 +0100

    JENKINS-39076 Do not call super to workaround Groovy StackOverflowError bug.
    Utilize StaplerRequest.bindJSON method as per the new implementation of GlobalConfiguration in Jenkins 2.x. bindJSON method requires setter methods, hence opening the setter methods visibility from package to public.

--- src/main/groovy/com/ceilfors/jenkins/plugins/jiratrigger/JiraTriggerGlobalConfiguration.groovy
+++ src/main/groovy/com/ceilfors/jenkins/plugins/jiratrigger/JiraTriggerGlobalConfiguration.groovy
@@ -1,76 +1,45 @@
 package com.ceilfors.jenkins.plugins.jiratrigger
 
-import groovy.transform.PackageScope
 import hudson.Extension
 import hudson.util.Secret
 import jenkins.model.GlobalConfiguration
 import net.sf.json.JSONObject
 import org.kohsuke.stapler.StaplerRequest
+
 /**
  * @author ceilfors
  */
 @Extension
 class JiraTriggerGlobalConfiguration extends GlobalConfiguration {
 
-    private String jiraRootUrl
-    private String jiraUsername
+    String jiraRootUrl
+    String jiraUsername
     private Secret jiraPassword
-    private boolean jiraCommentReply = false
+    boolean jiraCommentReply = false
 
     JiraTriggerGlobalConfiguration() {
         load()
     }
 
     JiraTriggerGlobalConfiguration(String jiraRootUrl, String jiraUsername, String jiraPassword) {
-        setJiraRootUrl(jiraRootUrl)
-        setJiraUsername(jiraUsername)
-        setJiraPassword(jiraPassword)
+        this.jiraRootUrl = jiraRootUrl
+        this.jiraUsername = jiraUsername
+        this.setJiraPassword(jiraPassword)
     }
 
     @Override
     boolean configure(StaplerRequest req, JSONObject formData) {
-        setJiraRootUrl(formData.getString("jiraRootUrl"))
-        setJiraUsername(formData.getString("jiraUsername"))
-        setJiraPassword(formData.getString("jiraPassword"))
-        jiraCommentReply = formData.getBoolean("jiraCommentReply")
+        req.bindJSON(this, formData)
         save();
-        return super.configure(req, formData)
-    }
-
-    String getJiraRootUrl() {
-        return jiraRootUrl
-    }
-
-    String getJiraUsername() {
-        return jiraUsername
+        return true;
     }
 
     Secret getJiraPassword() {
         return jiraPassword
     }
 
-    boolean getJiraCommentReply() {
-        return jiraCommentReply
-    }
-
-    @PackageScope
-    void setJiraRootUrl(String jiraRootUrl) {
-        this.jiraRootUrl = jiraRootUrl
-    }
-
-    @PackageScope
-    void setJiraUsername(String jiraUsername) {
-        this.jiraUsername = jiraUsername
-    }
-
-    @PackageScope
     void setJiraPassword(String jiraPassword) {
         this.jiraPassword = Secret.fromString(jiraPassword)
-    }
-
-    @PackageScope
-    void setJiraCommentReply(boolean jiraCommentReply) {
-        this.jiraCommentReply = jiraCommentReply
     }
 
     void validateConfiguration() {
