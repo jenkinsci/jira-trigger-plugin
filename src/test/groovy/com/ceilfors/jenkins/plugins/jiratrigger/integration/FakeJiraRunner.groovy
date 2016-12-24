ab2843563369b46fc6fbb40694f81919c5ac6070
Wisen Tanasa <wisen@ceilfors.com>
2016-12-24 09:28:11 +0000

    JENKINS-40361 Stores description during issue creation for webhook usages.

--- src/test/groovy/com/ceilfors/jenkins/plugins/jiratrigger/integration/FakeJiraRunner.groovy
+++ src/test/groovy/com/ceilfors/jenkins/plugins/jiratrigger/integration/FakeJiraRunner.groovy
@@ -3,32 +3,38 @@ package com.ceilfors.jenkins.plugins.jiratrigger.integration
 import groovy.json.JsonSlurper
 import groovyx.net.http.ContentType
 import groovyx.net.http.RESTClient
-
 /**
  * @author ceilfors
  */
 class FakeJiraRunner implements JiraRunner {
 
     private RESTClient restClient
+    private int id = 1;
+    private Map<String, FakeIssue> issueMap = [:]
 
-    FakeJiraRunner(String webhookUrl) {
-        restClient = createRestClient(webhookUrl)
+    FakeJiraRunner(JenkinsRunner jenkinsRunner) {
+        restClient = createRestClient(jenkinsRunner.webhookUrl)
     }
 
     @Override
     String createIssue() {
-        return 'TEST-1'
+        return createIssue('')
     }
 
     @Override
     String createIssue(String description) {
-        return 'TEST-1'
+        String issueKey = "TEST-$id"
+        id++
+        issueMap[issueKey] = new FakeIssue(issueKey: issueKey, description: description)
+        return issueKey
     }
 
     @Override
     void updateDescription(String issueKey, String description) {
+        issueMap[issueKey].description = description
         Map body = new JsonSlurper().parse(new FileReader(new File(this.class.getResource('updateDescription.json').toURI()))) as Map
         body.issue.key = issueKey
+        body.issue.fields.description = issueMap[issueKey].description
         body.changelog.items[0].toString = description
         restClient.post(body: body)
     }
@@ -37,6 +43,7 @@ class FakeJiraRunner implements JiraRunner {
     void updateStatus(String issueKey, String status) {
         Map body = new JsonSlurper().parse(new FileReader(new File(this.class.getResource('updateStatus.json').toURI()))) as Map
         body.issue.key = issueKey
+        body.issue.fields.description = issueMap[issueKey].description
         body.changelog.items[0].toString = status
         restClient.post(body: body)
     }
@@ -50,6 +57,7 @@ class FakeJiraRunner implements JiraRunner {
     void updateCustomField(String issueKey, String fieldName, String value) {
         Map body = new JsonSlurper().parse(new FileReader(new File(this.class.getResource('updateCustomField.json').toURI()))) as Map
         body.issue.key = issueKey
+        body.issue.fields.description = issueMap[issueKey].description
         body.changelog.items[0].field = fieldName
         body.changelog.items[0].toString = value
         restClient.post(body: body)
@@ -59,16 +67,22 @@ class FakeJiraRunner implements JiraRunner {
     void addComment(String issueKey, String comment) {
         Map body = new JsonSlurper().parse(new FileReader(new File(this.class.getResource('addComment.json').toURI()))) as Map
         body.issue.key = issueKey
+        body.issue.fields.description = issueMap[issueKey].description
         body.comment.body = comment
         restClient.post(body: body)
     }
 
     @Override
     boolean validateIssueKey(String issueKey, String jqlFilter) {
-        return false
+        return true
     }
 
     private RESTClient createRestClient(String jenkinsUrl) {
-        new RESTClient(jenkinsUrl, ContentType.JSON)
+        return new RESTClient(jenkinsUrl, ContentType.JSON)
+    }
+
+    private class FakeIssue {
+        String issueKey
+        String description
     }
 }
--- src/test/groovy/com/ceilfors/jenkins/plugins/jiratrigger/integration/FakeJiraSetupRule.groovy
+++ src/test/groovy/com/ceilfors/jenkins/plugins/jiratrigger/integration/FakeJiraSetupRule.groovy
@@ -1,6 +1,5 @@
 package com.ceilfors.jenkins.plugins.jiratrigger.integration
 
-import com.ceilfors.jenkins.plugins.jiratrigger.JiraCommentReplier
 import com.ceilfors.jenkins.plugins.jiratrigger.jira.JiraClient
 import org.junit.rules.ExternalResource
 
@@ -13,28 +12,16 @@ import static com.ceilfors.jenkins.plugins.jiratrigger.JiraCommentTrigger.JiraCo
 class FakeJiraSetupRule extends ExternalResource {
 
     JenkinsRunner jenkinsRunner
+    JiraClient jiraClient
 
-    FakeJiraSetupRule(JenkinsRunner jenkinsRunner) {
+    FakeJiraSetupRule(JenkinsRunner jenkinsRunner, JiraClient jiraClient) {
         this.jenkinsRunner = jenkinsRunner
+        this.jiraClient = jiraClient
     }
 
     protected void before() throws Throwable {
-        def jiraClient = new FakeJiraClient()
-
         // KLUDGE: Could not find a better way to override Guice injection
         jenkinsRunner.jenkins.getDescriptorByType(JiraChangelogTriggerDescriptor).jiraClient = jiraClient
         jenkinsRunner.jenkins.getDescriptorByType(JiraCommentTriggerDescriptor).jiraClient = jiraClient
-    }
-
-    static class FakeJiraClient implements JiraClient {
-
-        @Override
-        void addComment(String issueKey, String comment) {
-        }
-
-        @Override
-        boolean validateIssueKey(String issueKey, String jqlFilter) {
-            return true
-        }
     }
 }
