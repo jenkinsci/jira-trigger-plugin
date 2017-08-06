a9544730e87943f8fb5322f243daa0565a7d7864
Wisen Tanasa <wisen@ceilfors.com>
2017-08-06 10:02:34 +0100

    JENKINS-43642 Add failing test.

--- src/test/groovy/com/ceilfors/jenkins/plugins/jiratrigger/JiraTriggerTest.groovy
+++ src/test/groovy/com/ceilfors/jenkins/plugins/jiratrigger/JiraTriggerTest.groovy
@@ -2,7 +2,9 @@ package com.ceilfors.jenkins.plugins.jiratrigger
 
 import hudson.model.FreeStyleProject
 import org.junit.Rule
+import org.jvnet.hudson.test.Issue
 import org.jvnet.hudson.test.JenkinsRule
+import spock.lang.Ignore
 import spock.lang.Specification
 
 import static com.ceilfors.jenkins.plugins.jiratrigger.JiraCommentTrigger.JiraCommentTriggerDescriptor
@@ -45,6 +47,20 @@ class JiraTriggerTest extends Specification {
         def trigger = new JiraCommentTrigger()
         trigger.start(job, true)
         job.renameTo('newjob')
+        trigger.stop()
+
+        then:
+        descriptor.allTriggers().size() == 0
+    }
+
+    @Issue('JENKINS-43642')
+    @Ignore
+    def 'Should be able to stop JiraTrigger when the trigger is not started yet'() {
+        setup:
+        JiraCommentTriggerDescriptor descriptor = jenkinsRule.instance.getDescriptor(JiraCommentTrigger)
+
+        when:
+        def trigger = new JiraCommentTrigger()
         trigger.stop()
 
         then:
